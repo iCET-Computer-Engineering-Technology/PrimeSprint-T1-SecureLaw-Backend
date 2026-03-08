10b9537af80ff81aba7a1096a3754f46f95c94ed
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-08 23:57:50 +0530

    Add roles table and update users table to reference roles

--- src/main/resources/db/migration/V1__auth_table.sql
+++ src/main/resources/db/migration/V1__auth_table.sql
@@ -1,12 +1,30 @@
-CREATE TABLE users (
+CREATE TABLE roles (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL DEFAULT 'ACTIVE',
-    seniorId UUID,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    foreign key (seniorId) references users(id)
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+
+INSERT INTO roles (name) VALUES
+     ('SENIOR'),
+     ('JUNIOR')
+;
+CREATE TABLE users (
+       id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+       username VARCHAR(50) NOT NULL UNIQUE,
+       email VARCHAR(255) NOT NULL UNIQUE,
+
+       password VARCHAR(255) NOT NULL,
+
+       role_id UUID NOT NULL,
+
+       status VARCHAR(20) NOT NULL DEFAULT 'ACTIVE',
+
+       senior_id UUID,
+
+       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+       CONSTRAINT fk_user_role FOREIGN KEY (role_id) REFERENCES roles(id),
+
+       CONSTRAINT fk_user_senior FOREIGN KEY (senior_id) REFERENCES users(id)
 );
