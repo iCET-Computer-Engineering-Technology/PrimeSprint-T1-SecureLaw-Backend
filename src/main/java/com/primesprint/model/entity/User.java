c38ac645273ce69d235aa1784356e1c1cecec7f9
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-11 00:28:05 +0530

    Refactor User entity to use Timestamp for createdAt and updatedAt fields; add roleId for enhanced role management

--- src/main/java/com/primesprint/model/entity/User.java
+++ src/main/java/com/primesprint/model/entity/User.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -22,10 +23,11 @@ public class User implements UserDetails {
     private String email;
     private String password;
     private Role role;
+    private UUID roleId;
     private String status;
     private UUID seniorId;
-    private Date createdAt;
-    private Date updatedAt;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
