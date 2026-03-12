70e5f0940db790456a03b41c87bf72a7566d9375
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-03-12 17:11:41 +0530

    Remove DataSourceAutoConfiguration exclusion from SpringBootApplication annotation

--- src/main/java/com/primesprint/Main.java
+++ src/main/java/com/primesprint/Main.java
@@ -2,9 +2,8 @@ package com.primesprint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
