468b6f83b2ebc2e959f518bd3031f972f8a8782b
rash-ad <Rashad.mohd2004@gmail.com>
2026-03-18 11:47:42 +0530

    Enabled communication between Angular (localhost:4200) and Spring Boot backend

--- src/main/java/com/primesprint/controller/ChatController.java
+++ src/main/java/com/primesprint/controller/ChatController.java
@@ -7,7 +7,7 @@ import com.primesprint.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/api/chat")
 @RequiredArgsConstructor
