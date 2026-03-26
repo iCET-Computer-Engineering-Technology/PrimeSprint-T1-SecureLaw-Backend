5591b6db5f8ae71d3882e728b70acdc4820f7a51
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-03-26 12:01:31 +0530

    feat: update JwtUtil to generate tokens with unique jti and user roles

--- src/main/java/com/primesprint/security/JwtUtil.java
+++ src/main/java/com/primesprint/security/JwtUtil.java
@@ -1,17 +1,19 @@
 package com.primesprint.security;
 
 import com.primesprint.config.JwtProperties;
+import com.primesprint.model.entity.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Date;
+import java.util.*;
 import java.util.function.Function;
 
 
@@ -57,12 +59,21 @@ public class JwtUtil {
     }
 
     // Generate JWT token
-    public String generateToken(UserDetails userDetails) {
+    public String generateToken(User user) {
 
         long ttl = jwtProperties.getAccess().getToken().getTtl();
+        String jti = UUID.randomUUID().toString();// Add jti for uniqueness if needed
+
+        Map<String,Object> calims = new HashMap<>();
+        calims.put("roles", user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList());
 
         return Jwts.builder()
-                .setSubject(userDetails.getUsername())
+                .setClaims(calims)
+                .setSubject(user.getUsername())
+                .setId(jti)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + ttl))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
@@ -80,5 +91,13 @@ public class JwtUtil {
         } catch (JwtException e) {
             return false;
         }
+    }
+
+    public String extractJti(String token) {
+        return extractClaim(token, Claims::getId);
+    }
+
+    public List<String> extractRoles(String token) {
+        return extractClaim(token, claims -> (List<String>) claims.get("roles"));
     }
 }
